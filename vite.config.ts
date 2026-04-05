b289ef5695d9bed2605e4b09d8b4e10b26489737
Eric <ericc@example.com>
2026-04-05 11:41:19 +1000

    Configure Vite to build assets to root directory for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -9,9 +9,9 @@ export default defineConfig({
     outDir: 'dist',
     rollupOptions: {
       output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
+        entryFileNames: '[name].js',
+        chunkFileNames: '[name].js',
+        assetFileNames: '[name].[ext]'
       }
     }
   }
